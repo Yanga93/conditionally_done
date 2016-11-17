4c25475f0c232793375d9da47f3c6cf6d16c626b
Yanga93 <yangazukelwa@gmail.com>
2016-11-17 13:51:04 +0200

    Added switch method to excute my function

--- fromWhere.js
+++ fromWhere.js
@@ -1,5 +1,30 @@
 const assert = require('assert');
 var fromWhere = function(regNum){
+
+ switch(regNum.substring(0,2)){
+ case 'CY':
+   return 'Bellville';
+     break;
+
+ case 'CJ':
+   return 'Paarl';
+     break;
+
+ case 'CA':
+   return 'Cape Town';
+     break;
+
+ default:
+       return 'Some other place!';
+   }
+ };
+ //console.log(fromWhere('GP 2536'));
+   assert.equal(fromWhere('CJ'), 'Paarl');
+
+//Using if and else statements condition
+/*
+const assert = require('assert');
+var fromWhere = function(regNum){
   if (regNum.startsWith('CY') === true){
     console.log('Bellville');
       return 'Bellville';
@@ -18,5 +43,5 @@ var fromWhere = function(regNum){
           return 'Some other place!';
   }
 };
-assert.equal(fromWhere('CJ'), 'Paarl');
+assert.equal(fromWhere('CJ'), 'Paarl');*/
 //assert.equal(fromWhere('GP'), 'Some other place!');
